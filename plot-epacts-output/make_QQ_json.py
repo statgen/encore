ceb195c2c5aaae55fef7cda47437f63ab7194580
Peter VandeHaar <pjvandehaar@gmail.com>
2016-05-12 16:00:29 -0400

    Add python scripts to create json files for Manhattan and QQ plots

---
+++ plot-epacts-output/make_QQ_json.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python2
+
+'''
+This script takes two arguments:
+- an input filename (the output of epacts with a single phenotype)
+- an output filename (please end it with `.json`)
+
+It creates a json file which can be used to render a QQ plot.
+'''
+
+from __future__ import print_function, division, absolute_import
+
+import re
+import json
+import math
+import datetime
+# import scipy.stats
+import collections
+
+data_dir = '/var/pheweb_data/'
+NEGLOG10_PVAL_BIN_SIZE = 0.05 # Use 0.05, 0.1, 0.15, etc
+NEGLOG10_PVAL_BIN_DIGITS = 2 # Then keep 2 digits after the decimal
+NUM_BINS = 1000
+
+NUM_MAF_RANGES = 4 # Split MAF into 4 equally-sized ranges
+
+
+def round_sig(x, digits):
+    return 0 if x==0 else round(x, digits-1-int(math.floor(math.log10(abs(x)))))
+assert round_sig(0.00123, 2) == 0.0012
+assert round_sig(1.59e-10, 2) == 1.6e-10
+
+
+def parse_marker_id(marker_id):
+    try:
+        chr1, pos1, ref, alt, chr2, pos2 = re.match(r'([^:]+):([0-9]+)_([-ATCG]+)/([-ATCG]+)_([^:]+):([0-9]+)', marker_id).groups()
+        assert chr1 == chr2
+        assert pos1 == pos2
+    except:
+        print(marker_id)
+        raise
+    return chr1, int(pos1), ref, alt
+
+
+def approx_equal(a, b, tolerance=1e-4):
+    return abs(a-b) <= max(abs(a), abs(b)) * tolerance
+assert approx_equal(42, 42.0000001)
+assert not approx_equal(42, 42.01)
+
+
+# def gc_value(median_pval):
+#     # This should be equivalent to this R: `qchisq(p, df=1, lower.tail=F) / qchisq(.5, df=1, lower.tail=F)`
+#     return scipy.stats.chi2.ppf(1 - median_pval, 1) / scipy.stats.chi2.ppf(0.5, 1)
+# assert approx_equal(gc_value(0.49), 1.047457) # I computed these using that R code.
+# assert approx_equal(gc_value(0.5), 1)
+# assert approx_equal(gc_value(0.50001), 0.9999533)
+# assert approx_equal(gc_value(0.6123), 0.5645607)
+
+
+Variant = collections.namedtuple('Variant', ['neglog10_pval', 'maf'])
+def parse_variant_line(variant_line):
+    v = variant_line.split('\t')
+    assert v[1] == v[2]
+    if v[8] == 'NA' or v[9] == 'NA':
+        assert v[8] == 'NA' and v[9] == 'NA'
+    else:
+        chrom, pos, maf, pval, beta, sebeta = v[0], int(v[1]), float(v[7]), float(v[8]), float(v[9]), float(v[10])
+        chrom2, pos2, ref, alt = parse_marker_id(v[3])
+        assert chrom == chrom2
+        assert pos == pos2
+        return Variant(-math.log10(pval), maf)
+
+
+def rounded(x):
+    return round(x // NEGLOG10_PVAL_BIN_SIZE * NEGLOG10_PVAL_BIN_SIZE, NEGLOG10_PVAL_BIN_DIGITS)
+
+Variant = collections.namedtuple('Variant', ['neglog10_pval', 'maf'])
+def make_qq_stratified(variant_lines):
+    variants = []
+    for variant_line in variant_lines:
+        variant = parse_variant_line(variant_line)
+        if variant is not None:
+            variants.append(variant)
+    variants = sorted(variants, key=lambda v: v.maf)
+
+    # QQ
+    num_variants_in_biggest_maf_range = int(math.ceil(len(variants) / NUM_MAF_RANGES))
+    max_exp_neglog10_pval = -math.log10(0.5 / num_variants_in_biggest_maf_range) #expected
+    max_obs_neglog10_pval = max(v.neglog10_pval for v in variants) #observed
+    # TODO: should max_obs_neglog10_pval be at most 9?  That'd break our assertions.
+    # print(max_exp_neglog10_pval, max_obs_neglog10_pval)
+
+    qqs = [dict() for i in range(NUM_MAF_RANGES)]
+    for qq_i in range(NUM_MAF_RANGES):
+        slice_indices = (len(variants) * qq_i//4,
+                         len(variants) * (qq_i+1)//NUM_MAF_RANGES - 1)
+        qqs[qq_i]['maf_range'] = (variants[slice_indices[0]].maf,
+                               variants[slice_indices[1]].maf)
+
+        neglog10_pvals = sorted((v.neglog10_pval for v in variants[slice(*slice_indices)]),
+                                reverse=True)
+
+        occupied_bins = set()
+        for i, obs_neglog10_pval in enumerate(neglog10_pvals):
+            exp_neglog10_pval = -math.log10( (i+0.5) / len(neglog10_pvals))
+            exp_bin = int(exp_neglog10_pval / max_exp_neglog10_pval * NUM_BINS)
+            obs_bin = int(obs_neglog10_pval / max_obs_neglog10_pval * NUM_BINS)
+            occupied_bins.add( (exp_bin,obs_bin) )
+        # print(sorted(occupied_bins))
+
+        qq = []
+        for exp_bin, obs_bin in occupied_bins:
+            assert 0 <= exp_bin <= NUM_BINS, exp_bin
+            assert 0 <= obs_bin <= NUM_BINS, obs_bin
+            qq.append((
+                exp_bin / NUM_BINS * max_exp_neglog10_pval,
+                obs_bin / NUM_BINS * max_obs_neglog10_pval
+            ))
+        qq = sorted(qq)
+
+        qqs[qq_i]['qq'] = qq
+
+    return qqs
+
+'''
+def make_qq(variants):
+    neglog10_pvals = []
+    for variant in variants:
+        chrom, pos, ref, alt, maf, pval = parse_variant_tuple(variant)
+        neglog10_pvals.append(-math.log10(pval))
+    neglog10_pvals = sorted(neglog10_pvals)
+
+    # QQ
+    max_exp_neglog10_pval = -math.log10(1/len(neglog10_pvals)) #expected
+    max_obs_neglog10_pval = max(neglog10_pvals) #observed
+    # print(max_obs_neglog10_pval, max_exp_neglog10_pval)
+
+    occupied_bins = set()
+    for i, obs_neglog10_pval in enumerate(neglog10_pvals):
+        exp_neglog10_pval = -math.log10((len(neglog10_pvals)-i)/len(neglog10_pvals))
+        exp_bin = int(exp_neglog10_pval / max_exp_neglog10_pval * NUM_BINS)
+        obs_bin = int(obs_neglog10_pval / max_obs_neglog10_pval * NUM_BINS)
+        occupied_bins.add( (exp_bin,obs_bin) )
+    # print(sorted(occupied_bins))
+
+    qq = []
+    for exp_bin, obs_bin in occupied_bins:
+        assert exp_bin <= NUM_BINS, exp_bin
+        assert obs_bin <= NUM_BINS, obs_bin
+        qq.append((
+            exp_bin / NUM_BINS * max_exp_neglog10_pval,
+            obs_bin / NUM_BINS * max_obs_neglog10_pval
+        ))
+    qq = sorted(qq)
+
+    # GC_value lambda
+    median_neglog10_pval = neglog10_pvals[len(neglog10_pvals)//2]
+    median_pval = 10 ** -median_neglog10_pval # I know, `10 ** -(-math.log10(pval))` is gross.
+    gc_value_lambda = gc_value(median_pval)
+
+    rv = {
+        'qq': qq,
+        'median_pval': median_pval,
+        'gc_value_lambda': round_sig(gc_value_lambda, 5),
+    }
+    return rv
+'''
+
+
+
+epacts_filename = sys.argv[1]
+assert os.path.exists(epacts_filename)
+out_filename = sys.argv[2]
+assert os.path.exists(os.path.dirname(out_filename))
+
+with open(epacts_filename) as f:
+    header = f.readline().rstrip('\n').split('\t')
+    assert header == ['#CHROM', 'BEGIN', 'END', 'MARKER_ID', 'NS', 'AC', 'CALLRATE', 'MAF', 'PVALUE', 'BETA', 'SEBETA', 'TSTAT', 'R2']
+
+    variant_lines = (line.rstrip('\n') for line in f)
+    rv = make_qq_stratified(variant_lines)
+
+with open(out_filename, 'w') as f:
+    json.dump(rv, f, sort_keys=True, indent=0)
+print('{} -> {}'.format(epacts_filename, out_filename))
---
+++ plot-epacts-output/make_manattan_json.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python2
+
+'''
+This script takes two arguments:
+- an input filename (the output of epacts with a single phenotype)
+- an output filename (please end it with `.json`)
+
+It creates a json file which can be used to render a Manhattan plot.
+'''
+
+from __future__ import print_function, division, absolute_import
+
+import re
+import json
+import math
+import collections
+import sys
+import os.path
+
+BIN_LENGTH = int(3e6)
+NEGLOG10_PVAL_BIN_SIZE = 0.05 # Use 0.05, 0.1, 0.15, etc
+NEGLOG10_PVAL_BIN_DIGITS = 2 # Then round to this many digits
+BIN_THRESHOLD = 1e-4 # pvals less than this threshold don't get binned.
+
+def round_sig(x, digits):
+    return 0 if x==0 else round(x, digits-1-int(math.floor(math.log10(abs(x)))))
+assert round_sig(0.00123, 2) == 0.0012
+assert round_sig(1.59e-10, 2) == 1.6e-10
+
+
+def parse_marker_id(marker_id):
+    try:
+        chr1, pos1, ref, alt, chr2, pos2 = re.match(r'([^:]+):([0-9]+)_([-ATCG]+)/([-ATCG]+)_([^:]+):([0-9]+)', marker_id).groups()
+        assert chr1 == chr2
+        assert pos1 == pos2
+    except:
+        print(marker_id)
+        raise
+    return chr1, int(pos1), ref, alt
+
+Variant = collections.namedtuple('Variant', 'chrom pos ref alt maf pval beta sebeta'.split())
+def parse_variant_line(variant_line):
+    v = variant_line.split('\t')
+    assert v[1] == v[2]
+    if v[8] == 'NA' or v[9] == 'NA':
+        assert v[8] == 'NA' and v[9] == 'NA'
+    else:
+        chrom, pos, maf, pval, beta, sebeta = v[0], int(v[1]), float(v[7]), float(v[8]), float(v[9]), float(v[10])
+        chrom2, pos2, ref, alt = parse_marker_id(v[3])
+        assert chrom == chrom2
+        assert pos == pos2
+        return Variant(chrom, pos, ref, alt, maf, pval, beta, sebeta)
+
+def rounded_neglog10(pval):
+    return round(-math.log10(pval) // NEGLOG10_PVAL_BIN_SIZE * NEGLOG10_PVAL_BIN_SIZE, NEGLOG10_PVAL_BIN_DIGITS)
+
+def get_pvals_and_pval_extents(pvals):
+    # expects that NEGLOG10_PVAL_BIN_SIZE is the distance between adjacent bins.
+    pvals = sorted(pvals)
+    extents = [[pvals[0], pvals[0]]]
+    for p in pvals:
+        if extents[-1][1] + NEGLOG10_PVAL_BIN_SIZE * 1.1 > p:
+            extents[-1][1] = p
+        else:
+            extents.append([p,p])
+    rv_pvals, rv_pval_extents = [], []
+    for (start, end) in extents:
+        if start == end:
+            rv_pvals.append(start)
+        else:
+            rv_pval_extents.append([start,end])
+    return (rv_pvals, rv_pval_extents)
+
+def bin_variants(variant_lines):
+    bins = []
+    unbinned_variants = []
+
+    prev_chrom, prev_pos = -1, -1
+    for variant_line in variant_lines:
+        variant = parse_variant_line(variant_line)
+        if variant is None: continue
+        assert variant.pos >= prev_pos or int(variant.chrom) > int(prev_chrom), (variant.chrom, variant.pos, prev_chrom, prev_pos)
+        prev_chrom, prev_pos = variant.chrom, variant.pos
+
+        if variant.pval < BIN_THRESHOLD:
+            unbinned_variants.append({
+                'chrom': variant.chrom,
+                'pos': variant.pos,
+                'ref': variant.ref,
+                'alt': variant.alt,
+                'maf': round_sig(variant.maf, 3),
+                'pval': round_sig(variant.pval, 2),
+                'beta': round_sig(variant.beta, 2),
+                'sebeta': round_sig(variant.sebeta, 2),
+            })
+
+        else:
+            if len(bins) == 0 or variant.chrom != bins[-1]['chrom']:
+                # We need a new bin, starting with this variant.
+                bins.append({
+                    'chrom': variant.chrom,
+                    'startpos': variant.pos,
+                    'neglog10_pvals': set(),
+                })
+            elif variant.pos > bins[-1]['startpos'] + BIN_LENGTH:
+                # We need a new bin following the last one.
+                bins.append({
+                    'chrom': variant.chrom,
+                    'startpos': bins[-1]['startpos'] + BIN_LENGTH,
+                    'neglog10_pvals': set(),
+                })
+            bins[-1]['neglog10_pvals'].add(rounded_neglog10(variant.pval))
+
+    bins = [b for b in bins if len(b['neglog10_pvals']) != 0]
+    for b in bins:
+        b['neglog10_pvals'], b['neglog10_pval_extents'] = get_pvals_and_pval_extents(b['neglog10_pvals'])
+        b['pos'] = int(b['startpos'] + BIN_LENGTH/2)
+        del b['startpos']
+
+    return bins, unbinned_variants
+
+
+epacts_filename = sys.argv[1]
+assert os.path.exists(epacts_filename)
+out_filename = sys.argv[2]
+assert os.path.exists(os.path.dirname(out_filename))
+
+with open(epacts_filename) as f:
+    header = f.readline().rstrip('\n').split('\t')
+    assert header == ['#CHROM', 'BEGIN', 'END', 'MARKER_ID', 'NS', 'AC', 'CALLRATE', 'MAF', 'PVALUE', 'BETA', 'SEBETA', 'TSTAT', 'R2']
+
+    variant_lines = (line.rstrip('\n') for line in f)
+    variant_bins, unbinned_variants = bin_variants(variant_lines)
+
+rv = {
+    'variant_bins': variant_bins,
+    'unbinned_variants': unbinned_variants,
+}
+
+# Avoid getting killed while writing dest_filename, to stay idempotent despite me frequently killing the program
+with open(out_filename, 'w') as f:
+    json.dump(rv, f, sort_keys=True, indent=0)
+print('{} -> {}'.format(epacts_filename, out_filename))

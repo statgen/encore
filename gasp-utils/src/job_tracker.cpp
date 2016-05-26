ef22a1300dab4d9bd125687bc8dfd582c0bb3d71
jonathonl <jonathonl@users.noreply.github.com>
2016-05-26 12:50:47 -0400

    Switch job flags to use all chromosomes and entire node.

--- gasp-utils/src/job_tracker.cpp
+++ gasp-utils/src/job_tracker.cpp
@@ -170,10 +170,10 @@ void check_for_job_status_update(MYSQL* conn, const std::string& base_path, cons
           << " --vcf " << vcf_file
           << " --ped " << ped_file
           << " --min-maf 0.001 --field DS"
-          << " --chr 22"
+          << " --sepchr"
           << " --unit 500000 --test q.linear"
           << " --out " << epacts_output
-          << " --run 4";
+          << " --run 24";
 
           if (!ped_column_names.size())
           {
@@ -189,9 +189,9 @@ void check_for_job_status_update(MYSQL* conn, const std::string& base_path, cons
             ofs
             << "#!/bin/bash\n"
             << "#SBATCH --job-name=gasp_" << j.id() << "\n"
-            << "#SBATCH --ntasks-per-node=4\n"
+            << "#SBATCH --cpus-per-task=24\n"
             << "#SBATCH --workdir=" << job_directory << "\n"
-            << "#SBATCH --mem-per-cpu=4000\n"
+            << "#SBATCH --exclusive\n"
             //<< "#SBATCH --output=" << stdout_path << "\n"
             //<< "#SBATCH --error=" << stderr_path << "\n"
             //<< "#SBATCH --time=10:00\n"
@@ -293,18 +293,32 @@ void check_for_job_status_update(MYSQL* conn, const std::string& base_path, cons
     }
     else if (j.status() == job_status::cancel_requested)
     {
-      std::ifstream job_id_ifs(job_directory + "/batch_script_output.txt");
+      std::ifstream slurm_job_id_ifs(job_directory + "/batch_script_output.txt");
       //Submitted batch job xxxxxxxx
 
-      int job_id = 0;
-      while (job_id_ifs.good() && job_id == 0)
+      int slurm_job_id = 0;
+      while (slurm_job_id_ifs.good() && slurm_job_id == 0)
       {
-        job_id_ifs >> job_id;
+        slurm_job_id_ifs >> slurm_job_id;
       }
 
-      if (job_id)
-        std::system("scancel " + job_id);
-
+      if (!slurm_job_id)
+      {
+        //TODO: handle.
+      }
+      else
+      {
+        std::system(std::string("scancel " + std::to_string(slurm_job_id)).c_str());
+        std::string sql = "UPDATE jobs SET status_id = (SELECT id FROM statuses WHERE name='cancelled' LIMIT 1) WHERE id = uuid_to_bin('" + escape_string(conn, j.id()) + "')";
+        if (mysql_query(conn, sql.c_str()) != 0)
+        {
+          log(log_ofs, mysql_error(conn));
+        }
+        else
+        {
+          log(log_ofs, "Updated status to 'cancelled'.");
+        }
+      }
 
     }
   }

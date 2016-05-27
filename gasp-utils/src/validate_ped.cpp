a1103560deead8938da81bc4ba6549f6bd573016
jonathonl <jonathonl@users.noreply.github.com>
2016-05-27 10:03:32 -0400

    Temporarily remove PED validation.

--- gasp-utils/src/validate_ped.cpp
+++ gasp-utils/src/validate_ped.cpp
@@ -31,7 +31,7 @@ int validate_ped_file(const std::string& file_path, std::ostream& err_stream)
     else
     {
       ped_header_line.erase(0, ped_header_line.find_first_not_of(" #")); // Trim prefix
-      ped_header_line.erase(ped_header_line.find_last_not_of(" \r\n")+1); // Trim suffix
+      ped_header_line.erase(ped_header_line.find_last_not_of(" \r\n") + 1); // Trim suffix
 
       std::vector<std::string> column_names;
       column_names.reserve(10); // Approximate number of columns.
@@ -44,19 +44,18 @@ int validate_ped_file(const std::string& file_path, std::ostream& err_stream)
         column_names.push_back(std::move(tmp_column_name));
       }
 
-      long fam_id_offset = std::find(column_names.begin(), column_names.end(), "FAM_ID")-column_names.begin();
-      long ind_id_offset = std::find(column_names.begin(), column_names.end(), "IND_ID")-column_names.begin();
-      long dad_id_offset = std::find(column_names.begin(), column_names.end(), "DAD_ID")-column_names.begin();
-      long mom_id_offset = std::find(column_names.begin(), column_names.end(), "MOM_ID")-column_names.begin();
-      long sex_offset = std::find(column_names.begin(), column_names.end(), "SEX")-column_names.begin();
-      long age_offset = std::find(column_names.begin(), column_names.end(), "AGE")-column_names.begin();
-
-      if (fam_id_offset>=column_names.size()
-          || ind_id_offset>=column_names.size()
-          || dad_id_offset>=column_names.size()
-          || mom_id_offset>=column_names.size()
-          || sex_offset>=column_names.size()
-          || age_offset>=column_names.size())
+//      long fam_id_offset = std::find(column_names.begin(), column_names.end(), "FAM_ID")-column_names.begin();
+//      long ind_id_offset = std::find(column_names.begin(), column_names.end(), "IND_ID")-column_names.begin();
+//      long dad_id_offset = std::find(column_names.begin(), column_names.end(), "DAD_ID")-column_names.begin();
+//      long mom_id_offset = std::find(column_names.begin(), column_names.end(), "MOM_ID")-column_names.begin();
+//      long sex_offset = std::find(column_names.begin(), column_names.end(), "SEX")-column_names.begin();
+//
+//      if (fam_id_offset>=column_names.size()
+//          || ind_id_offset>=column_names.size()
+//          || dad_id_offset>=column_names.size()
+//          || mom_id_offset>=column_names.size()
+//          || sex_offset>=column_names.size())
+      if (false)
       {
         err_stream << "Missing required columns." << std::endl;
       }
@@ -69,7 +68,7 @@ int validate_ped_file(const std::string& file_path, std::ostream& err_stream)
         {
           ++line_number;
 
-          data_line.erase(data_line.find_last_not_of(" \r\n")+1);
+          data_line.erase(data_line.find_last_not_of(" \r\n") + 1);
 
           std::vector<std::string> fields_in_current_row;
           fields_in_current_row.reserve(column_names.size());
@@ -82,21 +81,20 @@ int validate_ped_file(const std::string& file_path, std::ostream& err_stream)
             fields_in_current_row.push_back(std::move(tmp_field));
           }
 
-          if (fields_in_current_row.size()!=column_names.size())
+          if (fields_in_current_row.size() != column_names.size())
           {
             invalid_row_found = true;
-            err_stream << "Number of fields in row does not match header (Line:"+std::to_string(line_number)+")."
-            << std::endl;
+            err_stream << "Number of fields in row does not match header (Line:" + std::to_string(line_number) + ")." << std::endl;
           }
-          else if (fields_in_current_row[sex_offset]!="1" && fields_in_current_row[sex_offset]!="2")
-          {
-            invalid_row_found = true;
-            err_stream << "SEX must to be '1' or '2' (Line:"+std::to_string(line_number)+")." << std::endl;
-          }
+//          else if (fields_in_current_row[sex_offset]!="1" && fields_in_current_row[sex_offset]!="2")
+//          {
+//            invalid_row_found = true;
+//            err_stream << "SEX must to be '1' or '2' (Line:"+std::to_string(line_number)+")." << std::endl;
+//          }
           else if (false) //!std::regex_match(fields_in_current_row[ind_id_offset], exp))
           {
             invalid_row_found = true;
-            err_stream << "Invalid ID format (Line:"+std::to_string(line_number)+")." << std::endl;
+            err_stream << "Invalid ID format (Line:" + std::to_string(line_number) + ")." << std::endl;
           }
 
         }
@@ -114,7 +112,7 @@ int main(int argc, char* argv[])
   int ret = -1;
   auto start_time = std::chrono::high_resolution_clock::now();
 
-  if (argc!=2)
+  if (argc != 2)
   {
     std::cerr << "Invalid number of args." << std::endl;
   }
@@ -130,8 +128,6 @@ int main(int argc, char* argv[])
     }
   }
 
-  std::cout
-  << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()-start_time).count()
-  << std::endl;
+  std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() << std::endl;
   return ret;
 }

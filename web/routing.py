4f6fab360e1ae936d022026616a6255681708f0d
jonathonl <jonathonl@users.noreply.github.com>
2016-08-16 17:32:28 -0400

    Started porting job tracking to python.

--- web/routing.py
+++ web/routing.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, session, send_from_directory, redirect
 import job_handlers
 import sign_in_handler
 import re
+import job_tracking
+import signal
 
 APP_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 APP_STATIC_PATH = os.path.join(APP_ROOT_PATH, 'static')
@@ -12,7 +14,6 @@ app = Flask(__name__)
 
 app.config.from_pyfile(os.path.join(APP_ROOT_PATH, "../flask_config.py"))
 app.config["PROPAGATE_EXCEPTIONS"] = True
-
 
 @app.route("/")
 def index():
@@ -127,6 +128,14 @@ def get_job_tmp_manhattan(job_id):
 # def internal_error(exception):
 #     return render_template('500.html'), 500
 
+job_tracker = job_tracking.Tracker(30.0, job_tracking.DatabaseCredentials("localhost", app.config.get("MYSQL_USER"), app.config.get("MYSQL_PASSWORD"), app.config.get("MYSQL_DB")))
+job_tracker.start()
+
+def sig_handler(signum, frame):
+    job_tracker.cancel()
+    exit(0)
+signal.signal(signal.SIGTERM, sig_handler)
+signal.signal(signal.SIGINT, sig_handler)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080, host="0.0.0.0");
+    app.run(debug=True, use_reloader=False, port=8080, host="0.0.0.0");

ce1175c00b642d75620213e5c5c2fb603f51ae5a
Matthew Flickinger <mflick@umich.edu>
2016-11-01 21:06:54 +0000

    JS Linting

--- web/static/js/job_details.js
+++ web/static/js/job_details.js
@@ -1,4 +1,7 @@
 
+/* eslint-env jquery */
+/* eslint no-unused-vars: ["error", { "vars": "local" }] */
+
 function init_job_tabs() {
     $("ul.tabs li").click(function()
     {
@@ -12,13 +15,13 @@ function init_job_tabs() {
 }
 
 function init_job_cancel_button(job_id, selector) {
-	selector = selector || "button[name=cancel_job]";
+    selector = selector || "button[name=cancel_job]";
     $(selector).click(function()
     {
         var xhr = new XMLHttpRequest();
-        xhr.addEventListener("load", function(ev)
+        xhr.addEventListener("load", function()
         {
-           location.reload();
+            location.reload();
         }, false);
 
         xhr.addEventListener("error", function() { alert("Request Failed"); }, false);
@@ -28,21 +31,20 @@ function init_job_cancel_button(job_id, selector) {
 }
 
 function init_manhattan(job_id, selector) {
-	selector = selector || "#tab1";
+    selector = selector || "#tab1";
     $.getJSON("/api/jobs/" + job_id + "/plots/manhattan").done(function(variants)
     {
         create_gwas_plot(selector, variants.variant_bins, variants.unbinned_variants, function(chrom, pos, ref, alt)
         {
-            console.log(chrom, pos, ref, alt);
-			jumpToLocusZoom(job_id, chrom, pos);
+            jumpToLocusZoom(job_id, chrom, pos);
         });
 
     });
 }
 
 function init_qqplot(job_id, selector, data_url) {
-	selector = selector || "#tab2";
-	data_url = data_url || "/api/jobs/" + job_id + "/plots/qq"; 
+    selector = selector || "#tab2";
+    data_url = data_url || "/api/jobs/" + job_id + "/plots/qq"; 
     $.getJSON(data_url).done(function(data)
     {
         /*_.sortBy(_.pairs(data.overall.gc_lambda)).forEach(function(d)
@@ -54,45 +56,46 @@ function init_qqplot(job_id, selector, data_url) {
 }
 
 function init_tophits(job_id, selector, data_url) {
-	selector = selector || "#tophits";
-	data_url = data_url|| "/api/jobs/" + job_id + "/tables/top"
-	$.getJSON(data_url).done(function(data) {
-		data = data.data || data
-		chrpos = {};
-		var i=0;
-		for(i=1; i<=22; i++) {
-			chrpos[i.toString()] = i
-		}
-		chrpos["X"] = i++;
-		chrpos["Y"] = i++;
-		chrpos["XY"] = i++;
-		chrpos["MT"] = i++;
-		for(var j=0; j<data.length; j++) {
-			chr = data[j].chrom.replace("chr","")
-			if (chrpos[chr] !== undefined) {
-				data[j].chrom_sort = chrpos[chr]
-			} else {
-				data[j].chrom_sort = chrpos[chr] = i++
-			}
-		}
+    selector = selector || "#tophits";
+    data_url = data_url|| "/api/jobs/" + job_id + "/tables/top";
+    $.getJSON(data_url).done(function(data) {
+        data = data.data || data;
+        var chrpos = {};
+        var i=0;
+        for(i=1; i<=22; i++) {
+            chrpos[i.toString()] = i;
+        }
+        chrpos["X"] = i++;
+        chrpos["Y"] = i++;
+        chrpos["XY"] = i++;
+        chrpos["MT"] = i++;
+        var chr;
+        for(var j=0; j<data.length; j++) {
+            chr = data[j].chrom.replace("chr","");
+            if (chrpos[chr] !== undefined) {
+                data[j].chrom_sort = chrpos[chr];
+            } else {
+                data[j].chrom_sort = chrpos[chr] = i++;
+            }
+        }
         var datacols = [
             {data: null, title:"Chrom",
                 orderData: [0,1],
                 type: "num",
                 render: function(data, type) {
                     if (type=="sort") {
-                        return data.chrom_sort
+                        return data.chrom_sort;
                     }
-                    return data.chrom
+                    return data.chrom;
                 },
                 className: "dt-body-right"
             },
             {data: "pos", title:"Position", 
                 render: function(data, type) {
                     if (type=="display") {
-                        return data.toLocaleString()
+                        return data.toLocaleString();
                     }
-                    return data
+                    return data;
                 },
                 orderData: [0,1],
                 className: "dt-body-right"
@@ -101,9 +104,9 @@ function init_tophits(job_id, selector, data_url) {
             {data: "pval", title:"Best P-Value", 
                 render: function(data, type) {
                     if (type=="display") {
-                        return data.toExponential(2)
+                        return data.toExponential(2);
                     }
-                    return data
+                    return data;
                 },
                 className: "dt-body-right"
             },
@@ -117,28 +120,28 @@ function init_tophits(job_id, selector, data_url) {
                 render:function(data, type, row) {
                     var fn = "event.preventDefault();" + 
                         "jumpToLocusZoom(\"" + job_id + "\",\"" + row.chrom + "\"," + data + ")";
-                    return "<a href='#' onclick='" + fn + "'>View</a>"
+                    return "<a href='#' onclick='" + fn + "'>View</a>";
                 },
                 orderable: false,
                 className: "dt-body-center"
             }
         ];
-		var table = $(selector).DataTable( {
-			data: data,
-			columns: datacols,
-			order: [[3, "asc"]],
-			lengthChange: false,
-			searching: false,
-			dom: 'rtip',
-		})
-		//$("#tophits").on("click","tr",function(event) {
-		//	var data = table.row(this).data()
-		//	jumpToLocusZoom(data.chrom, data.peak);
-		//})
-	}).fail(function() {
-		$("ul.tabs li[rel='tab3'").remove()
-	});
-};
+        $(selector).DataTable( {
+            data: data,
+            columns: datacols,
+            order: [[3, "asc"]],
+            lengthChange: false,
+            searching: false,
+            dom: "rtip"
+        });
+        //$("#tophits").on("click","tr",function(event) {
+        //  var data = table.row(this).data()
+        //  jumpToLocusZoom(data.chrom, data.peak);
+        //})
+    }).fail(function() {
+        $("ul.tabs li[rel='tab3'").remove();
+    });
+}
 
 function bin_chunks_by_chr_and_age(chunks, now) {
     var getAgeGroup = function(a) {
@@ -152,33 +155,33 @@ function bin_chunks_by_chr_and_age(chunks, now) {
         }
     };
     function startSort(a,b) {
-        return a.start - b.start
+        return a.start - b.start;
     }
     //http://machinesaredigging.com/2014/04/27/binary-insert-how-to-keep-an-array-sorted-as-you-insert-data-in-it/
     function binaryInsert(element, array, comp, start, end) {
-      start = (typeof start != "undefined") ? start : 0;
-      end = (typeof end != "undefined") ? end : array.length-1;
-      var pivot = start + Math.floor((end - start) / 2);
-      if (array.length==0) {array.push(value); return};
-      if (comp(element, array[end])>0) {
-        array.splice(end+1, 0, element); return;
-      }
-      if (comp(element, array[start])<0) {
-        array.splice(start, 0, element); return;
-      }
-      if (start >= end) {return;}
-      var c = comp(array[pivot], element);
-      if (c>0) {
-        binaryInsert(element, array, comp, start, pivot-1);
-      } else {
-        binaryInsert(element, array, comp, pivot+1, end);
-      }
-      return;
+        start = (typeof start != "undefined") ? start : 0;
+        end = (typeof end != "undefined") ? end : array.length-1;
+        var pivot = start + Math.floor((end - start) / 2);
+        if (array.length==0) {array.push(element); return;}
+        if (comp(element, array[end])>0) {
+            array.splice(end+1, 0, element); return;
+        }
+        if (comp(element, array[start])<0) {
+            array.splice(start, 0, element); return;
+        }
+        if (start >= end) {return;}
+        var c = comp(array[pivot], element);
+        if (c>0) {
+            binaryInsert(element, array, comp, start, pivot-1);
+        } else {
+            binaryInsert(element, array, comp, pivot+1, end);
+        }
+        return;
     }
     var bins = {};
     var group;
     chunks.forEach(function(chunk) {
-        modified = new Date(chunk.modified);
+        var modified = new Date(chunk.modified);
         var age = getAgeGroup(modified);
         group = "" + chunk.chr + "-" + age;
         if (!(group in bins)) {
@@ -191,13 +194,13 @@ function bin_chunks_by_chr_and_age(chunks, now) {
         }
     });
     return bins;
-};
+}
 
 function collapse_chunk_bins(bins) {
     var coll = [];
     function add(chr, age, start, stop, oldest, newest) {
         coll.push({chrom: chr, age: age, start: start, stop:stop,
-            oldest: oldest, newest: newest})
+            oldest: oldest, newest: newest});
     }
     var start;
     var stop;
@@ -210,7 +213,7 @@ function collapse_chunk_bins(bins) {
         newest = val.modified;
     }
     function extend(val) {
-        stop = val.stop
+        stop = val.stop;
         oldest = (val.modified<oldest) ? val.modified : oldest;
         newest = (val.modified>newest) ? val.modified : newest;
     }
@@ -219,7 +222,7 @@ function collapse_chunk_bins(bins) {
         reset(bin.vals[0]);
         for(var i=1; i<bin.vals.length; i++) {
             if (bin.vals[i].start != stop+1) {
-                add(bin.chrom, bin.age, start, stop, oldest, newest)
+                add(bin.chrom, bin.age, start, stop, oldest, newest);
                 reset(bin.vals[i]);
             } else {
                 extend(bin.vals[i]);
@@ -231,16 +234,16 @@ function collapse_chunk_bins(bins) {
 }
 
 function init_chunk_progress(job_id, selector) {
-	selector = selector || "#progress";
+    selector = selector || "#progress";
     $.getJSON("/api/jobs/" + job_id + "/chunks").done(function(resp) {
         var now = (resp.now && new Date(resp.now)) || new Date();
         chunks = resp.data || resp;
         if (chunks.length<1) {
             return;
-        };
+        }
         var x = bin_chunks_by_chr_and_age(chunks, now);
         var chunks = collapse_chunk_bins(x);
-        $(selector).append("<h3>Progress</h3>")
+        $(selector).append("<h3>Progress</h3>");
         var ideo = new Ideogram(selector);
         chunks = chunks.map(function(x) {
             //x.fill = ["#3CA661","#66F297","#1D5932"][x.age] ;
@@ -250,13 +253,13 @@ function init_chunk_progress(job_id, selector) {
         ideo.setRegions(chunks);
         ideo.draw();
     });
-};
+}
 
-jumpToLocusZoom = function(job_id, chr, pos) {
-	if (job_id && chr && pos) {
-		var region = chr + ":" + (pos-100000) + "-" + (pos+100000);
-		document.location.href = "/jobs/" + job_id + "/locuszoom/" + region;
-	}
+function jumpToLocusZoom(job_id, chr, pos) {
+    if (job_id && chr && pos) {
+        var region = chr + ":" + (pos-100000) + "-" + (pos+100000);
+        document.location.href = "/jobs/" + job_id + "/locuszoom/" + region;
+    }
 }
 
 

b38051f8cf77c654f10cb823d91d79537f515214
Matthew Flickinger <mflick@umich.edu>
2016-11-30 16:46:46 -0500

    Direct link to exac from LZ

--- web/static/js/job_locuszoom.js
+++ web/static/js/job_locuszoom.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
       .add("ld", ["LDEP", apiBase + "ld-"])
       .add("gene", ["GeneLZ", { url: apiBase + "gene", params: {source: 2} }])
       .add("recomb", ["RecombLZ", { url: apiBase + "recomb", params: {source: 15} }])
-      .add("constraint", ["GeneConstraintLZ", { url: apiBase + "constraint" }])
+      .add("constraint", ["GeneConstraintLZ", { url: "http://exac.broadinstitute.org/api/constraint" }])
       .add("sig", ["StaticJSON", [{ "x": 0, "y": 4.522 }, { "x": 2881033286, "y": 4.522 }] ]);
 
     LocusZoom.TransformationFunctions.set("scinotation", function(x) {

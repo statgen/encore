a2741fb2a74559303b902d7c2654829d78c1bc27
Matthew Flickinger <mflick@umich.edu>
2017-12-15 15:18:28 -0500

    Cleanup after rebase

--- web/chunk_progress.py
+++ web/chunk_progress.py
@@ -84,6 +84,8 @@ def get_chr_chunk_progress(output_file_glob, fileregex):
             m = p.search(file)
             chunk = dict(m.groupdict())
             chunk['chrom'] =  chunk['chr']
+            if not chunk['chrom'].startswith("chr"):
+                chunk['chrom'] = "chr" + chunk['chrom']
             chunk['start'] = int(chunk['start'])
             chunk['stop'] = int(chunk['stop'])
             chunk['modified'] = time.mktime(time.localtime(os.path.getmtime(file)))

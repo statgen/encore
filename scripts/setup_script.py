11a903f80102d7ccfec9001466f556001c5c3d21
Jacob Pleiness <pleiness@umich.edu>
2019-01-14 13:25:21 -0500

    MySQL setup working

--- scripts/setup_script.py
+++ scripts/setup_script.py
@@ -23,32 +23,47 @@ def install_packages():
     subprocess.call(['sudo', 'apt-get', 'update'])
     subprocess.call(['sudo', 'DEBIAN_FRONTEND=noninteractive', 'apt-get', 'install', '-y'] + packages)
 
+
 def setup_encore():
-    # Download and unpack Encore source
     subprocess.call(shlex.split('mkdir -p /srv/encore'))
     subprocess.call(shlex.split('curl -L https://github.com/statgen/encore/archive/master.zip --output /tmp/encore.zip'))
     subprocess.call(shlex.split('unzip /tmp/encore.zip -d /tmp/'))
     subprocess.call(shlex.split('cp -r /tmp/encore-master/. /srv/encore/'))
-    subprocess.call(shlex.split('rm -rf /tmp/'))
+    subprocess.call(shlex.split('rm -rf /tmp/encore.zip /tmp/encore-master'))
+
 
 def install_python_requirements():
-    # Install Encore requirements
     subprocess.call(['pip', 'install', '--upgrade', 'pip'])
-    #TODO make sure in right dir for command below
     subprocess.call(['pip', 'install', '-r', '/srv/encore/requirements.txt'])
 
+
 def setup_mysql():
-    pass
-    #TODO setup root password and apply schema
+    subprocess.call(['mkdir', '-p', '/var/lib/mysqld'])
+    subprocess.call(['chown', '-R', 'mysql:mysql', '/var/lib/mysqld'])
+    subprocess.call(['usermod', '-d', '/var/lib/mysql/', 'mysql'])
+    subprocess.call(shlex.split('sudo service mysql start'))
+    subprocess.call(['mysql', '-u', 'root', '-e',
+        "CREATE USER 'flask-user'@'localhost' IDENTIFIED BY 'flask-user-pass'"])
+    subprocess.call(['mysql', '-u', 'root', '-e',
+        "GRANT DELETE, INSERT, SELECT, UPDATE, EXECUTE ON encore.* TO 'flask-user'@'localhost'"])
+    subprocess.call(['mysql', '-u', 'root', '-e',
+        "DELETE FROM mysql.user WHERE User=''"])
+    subprocess.call(['mysql', '-u', 'root', '-e',
+        "FLUSH PRIVILEGES"])
+    subprocess.call(['mysql', '-u', 'root', '-e',
+        "ALTER USER 'root'@'localhost' IDENTIFIED BY 'test-pass'"])
+
 
 def setup_apache():
     pass
+
 
 def main():
     install_packages()
     setup_encore()
     install_python_requirements()
     setup_mysql()
+    setup_apache()
 
 
 if __name__ == '__main__':

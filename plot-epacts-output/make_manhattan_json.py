b47c83706e241aaa154b419a1c013fd0d0908d51
Matthew Flickinger <mflick@umich.edu>
2019-02-08 12:35:00 -0500

    Read files as text

--- plot-epacts-output/make_manhattan_json.py
+++ plot-epacts-output/make_manhattan_json.py
@@ -144,9 +144,9 @@ class AssocResultReader:
     def __enter__(self):
         if self.path and self.path != "-":
             if self.path.endswith(".gz"):
-                self.f = gzip.open(self.path)
+                self.f = gzip.open(self.path, "rt")
             else:
-                self.f = open(self.path)
+                self.f = open(self.path, "rt")
         else:
             self.f = sys.stdin
         return self

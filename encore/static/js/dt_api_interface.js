d58a5d6413f28a6b389ae57672ba8838355e5909
Matthew Flickinger <mflick@umich.edu>
2019-05-07 17:03:51 -0400

    Connect datatable to updated API

---
+++ encore/static/js/dt_api_interface.js
@@ -0,0 +1,84 @@
+/* global $ */
+/* exported encoreApi */
+// inspired by https://datatables.net/examples/server_side/pipeline.html
+
+function encoreApi(url) {
+    var cacheLastRequest = null;
+    var cacheLastJson = null;
+    var cache = {lower: -1, upper: -1};
+    var serverPageSize = 100;
+
+    function json_differ(a, b) {
+        return JSON.stringify( a ) !== JSON.stringify( b );
+    }
+
+    return function(request, drawCallback, settings) {
+        var apiOpts = { echo: request.draw };
+        if (request.start > 0) {
+            apiOpts.offset = request.start;
+            apiOpts.limit = serverPageSize;
+        }
+        var order_by = request.order.map(function(x) {
+            return ((x.dir=="desc")?"-":"") + request.columns[x.column].data;
+        }).join(",");
+        apiOpts.order_by = order_by;
+        if (request.search && request.search.value) {
+            apiOpts.q = request.search.value;
+        }
+
+        var needsFetching = false;
+        if ( cache.lower < 0 || request.start < cache.lower ||
+            (request.start + request.length) > cache.upper ) {
+
+            needsFetching = true;
+
+        } else if (json_differ(apiOpts.order_by, cacheLastRequest.order_by ) ||
+                   json_differ( apiOpts.q, cacheLastRequest.q ) )  {
+
+            needsFetching = true;
+        }
+
+        cacheLastRequest = $.extend( true, {}, apiOpts );
+
+        if (needsFetching) {
+            settings.jqXHR = $.ajax( {
+                "type": "GET",
+                "url": url,
+                "data": apiOpts,
+                "dataType": "json",
+                "cache": false,
+                "success": function(json) {
+                    if (json.header && typeof(json.header.total_count) !=="undefined" ) {
+                        //paged response
+                        json.draw = json.header.echo;
+                        json.recordsTotal = json.header.total_count;
+                        json.recordsFiltered = json.recordsTotal;
+
+                        cacheLastJson = $.extend(true, {}, json);
+                        cache.lower = json.header.offset;
+                        cache.upper = cache.lower + json.header.limit;
+                        serverPageSize = json.header.limit;
+
+                        json.data.splice( 0, request.start-cache.lower );
+                        json.data.splice( request.length, json.data.length );
+                        drawCallback(json);
+                    } else {
+                        //non-paged response, handle locally	
+                        json = { data: json };
+                        cache.lower = 0;
+                        cache.upper = json.data.length;
+                        settings.oFeatures.bServerSide = false;
+                        drawCallback(json);
+                    }
+                }
+            });
+        } else {
+            var json = $.extend( true, {}, cacheLastJson );
+            json.draw = request.draw; // Update the echo for each response
+            json.data.splice( 0, request.start-cache.lower );
+            json.data.splice( request.length, json.data.length );
+
+            drawCallback(json);
+        }
+    };
+}
